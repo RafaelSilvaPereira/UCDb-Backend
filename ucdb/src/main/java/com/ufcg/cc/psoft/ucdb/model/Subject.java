dfafee63359f80844fb375abbe2d8719d91676c3
MagnerSA <marcelo.andrade@ccc.ufcg.edu.br>
2019-06-13 11:22:24 -0300

    Test intellij

--- ucdb/src/main/java/com/ufcg/cc/psoft/ucdb/model/Subject.java
+++ ucdb/src/main/java/com/ufcg/cc/psoft/ucdb/model/Subject.java
@@ -1,4 +1,12 @@
 package com.ufcg.cc.psoft.ucdb.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Subject {
+
+    private String name;
+    @Id
+    private long id;
 }
