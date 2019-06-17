447e2058675306d14db84b8cca9eb3d5a06f9676
RafaelSilvaPereria <rafael.pereira@ccc.ufcg.edu.br>
2019-06-16 22:04:38 -0300

    test

--- ucdb/src/main/java/com/ufcg/cc/psoft/ucdb/UcdbApplication.java
+++ ucdb/src/main/java/com/ufcg/cc/psoft/ucdb/UcdbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UcdbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UcdbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UcdbApplication.class, args);
+    }
 
 }
